f677c19a5524f5618d06fe736ef1c7c488a586cc
“Yurii <j.koroliak@gmail.com>
2023-03-30 17:29:34 +0200

    HomeWorkJS1

---
+++ resources/js/main.js
@@ -0,0 +1,28 @@
+username=prompt('What is your name?');
+year=prompt('In what year were you born '+username+'?')
+const currentyear='2023'
+age=currentyear-year
+result=confirm('Your age is '+age)
+console.log(result)
+
+side=prompt('How many centimeters is one side?')
+resultsquare=(side*4)
+alert('Perimeter of your square is '+resultsquare+'cm')
+
+radius=prompt('How many cantimeters is radius?')
+console.log('the area of your circle is '+((radius**2)*3.14))
+
+distance=prompt('how much distance? (km)')
+time=prompt('How many time you want to spend? (hour)')
+speed=(distance/time)
+confirm('you have to move '+speed+'km/h')
+console.log(speed)
+
+// Commentary
+
+/* big 
+commentary
+wwqrd
+eewrd
+eeerd
+qqqrd*/
